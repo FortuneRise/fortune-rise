dfbff6e522b057bed3acee79656973d72473e96c
Jan <jr67464@student.uni-lj.si>
2024-12-23 18:39:25 +0100

    Added notification microservice

--- entities/src/main/resources/sql-scripts/init-db.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO wallet (id, balance) VALUES (1, 100.00);
-INSERT INTO wallet (id, balance) VALUES (2, 101.01);
-INSERT INTO user_table (id, name, surname, user_name, wallet_id) VALUES (1, 'Andraž', 'Hribernik', 'andh', 1);
-INSERT INTO user_table (id, name, surname, user_name, wallet_id) VALUES (2, 'Jan', 'Rutar', 'rut3r', 2);
