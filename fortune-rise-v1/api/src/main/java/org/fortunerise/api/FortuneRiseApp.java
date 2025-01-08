41328dbe2b9494a25246ac42333ec3ab524acac4
Andraž Hribernik <andraz.hrib@gmail.com>
2025-01-08 18:04:40 +0100

    Moved files into the root. Removed v1.

--- fortune-rise-v1/api/src/main/java/org/fortunerise/api/FortuneRiseApp.java
+++
@@ -1,8 +0,0 @@
-package org.fortunerise.api;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class FortuneRiseApp extends Application {
-}
--- fortune-rise-v1/services/src/main/java/org/fortunerise/beans/MrBean.java
+++
@@ -1,4 +0,0 @@
-package org.fortunerise.beans;
-
-public class MrBean {
-}
