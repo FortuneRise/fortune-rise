730f3c846e22b31b13cab5e054f60ba54608dcfe
Jan <jr67464@student.uni-lj.si>
2025-01-08 20:30:18 +0100

    Started with frontend

---
+++ fortune-rise-frontend/src/app/users/models/user.ts
@@ -0,0 +1,17 @@
+export class User{
+  id?: number;
+  name?: string;
+  surname?: string;
+  username?: string
+
+  /*
+  constructor(id:number,name:string,surname:string,username:string) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.username = username;
+
+  }
+
+   */
+}
---
+++ fortune-rise-frontend/src/app/users/services/users.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders}  from '@angular/common/http';
+import {catchError, Observable} from 'rxjs';
+import {User} from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+
+  private headers = new HttpHeaders({'Content-Type': 'application/json'});
+  private url = 'http://localhost:8080/api/users';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.url).pipe(catchError(this.handleError))
+  }
+
+  createUser(user: User):Observable<User>{
+    return this.http.post<User>(this.url,JSON.stringify(user), {headers: this.headers}).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Prišlo je do napake', error);
+    return Promise.reject(error.message || error);
+  }
+
+
+
+
+}
